7ebfbaecfab52a9a3b094e1b2ae2704db514f961
eduarda <e.qlourennco@gmail.com>
2024-09-06 10:14:20 -0300

    🎉 project basis

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-import { dados } from './dados.js';
-console.log(dados);
+
+
+function buscar() {
+    let section = document.getElementById("resultados");
+    let searchBox = document.getElementById("search-box").value;
+
+    if (!searchBox) {
+        section.innerHTML = "<p>Nada foi encontrado. Tente novamente com outro termo.</p>"
+        return
+    }
+
+    searchBox = searchBox.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let results = "";
+    let nome = ""; 
+    let descricao = "";
+    let tags = "";
+
+    for (let dado of dados) {
+        nome = dado.nome.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (nome.includes(searchBox) || descricao.includes(searchBox) || tags.includes(searchBox)) {
+            results += `
+                <div class="item-resultado">
+                    <div class="item-head">
+                        <img src="${dado.imagem}">
+                        <h2>
+                            <a href="#" target="_blank">${dado.nome}</a>
+                        </h2>
+                    </div>     
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank">Mais informações</a>
+                </div>
+            `;
+        }
+        if (!results) {
+            results = "<p>Nada foi encontrado</p>"
+        }
+        section.innerHTML = results;
+    }
+}
--- dados.js
+++ dados.js
@@ -1,20 +1,23 @@
-export let dados = [
+let dados = [
     {
         nome: 'Ada Lovelace',
         descricao: 'Ada Lovelace foi uma matemática e escritora britânica do século XIX, frequentemente reconhecida como a primeira programadora da história. Seus trabalhos pioneiros na área da computação abriram caminho para futuras gerações de programadoras e cientistas da computação.',
         link: 'https://pt.wikipedia.org/wiki/Ada_Lovelace',
-        imagem: 'https://media.gazetadopovo.com.br/2019/05/30072214/ada-ff48e782.jpg'
+        imagem: 'https://media.gazetadopovo.com.br/2019/05/30072214/ada-ff48e782.jpg',
+        tags: 'ada matematica escritora primeira programadora'
     },
     {
         nome: 'Grace Hopper',
         descricao: 'Pionera na área de programação, Grace Hopper foi uma das criadoras da linguagem de programação COBOL, uma das primeiras linguagens de alto nível, facilitando a comunicação entre humanos e computadores. Ela também é conhecida por popularizar o termo "bug" na computação, após encontrar uma mariposa presa em um relé de um computador.',
         link: 'https://pt.wikipedia.org/wiki/Grace_Hopper',
-        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzwu-MHrHZgbGequ8stJEKGTawcjHrNiBnaw&s'
+        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzwu-MHrHZgbGequ8stJEKGTawcjHrNiBnaw&s',
+        tags: 'programação programadora COBOL bug'
     },
     {
         nome: 'Hedy Lamarr',
         descricao: 'Além de sua carreira no cinema, Hedy Lamarr desenvolveu um sistema de comunicação de espectro espalhamento, um precursor da tecnologia utilizada em redes sem fio modernas como Wi-Fi e Bluetooth.',
         link: 'https://pt.wikipedia.org/wiki/Hedy_Lamarr',
-        imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Hedy_Lamarr_in_The_Heavenly_Body_1944.jpg/1200px-Hedy_Lamarr_in_The_Heavenly_Body_1944.jpg'
+        imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Hedy_Lamarr_in_The_Heavenly_Body_1944.jpg/1200px-Hedy_Lamarr_in_The_Heavenly_Body_1944.jpg',
+        tags: 'atriz wifi bluetooth'
     }
 ];
